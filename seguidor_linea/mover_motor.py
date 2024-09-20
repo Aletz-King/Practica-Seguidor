c7dd9884e0a32ebbece2b8ddb8e59d7f9e26534e
Said Alexander <reyesalexander210@gmail.com>
2024-09-19 19:04:21 -0600

    modificacines que dijo el doc con while

--- seguidor_linea/mover_motor.py
+++ seguidor_linea/mover_motor.py
@@ -21,15 +21,20 @@ def setup():
     GPIO.setup(line_pin_left,GPIO.IN)
     
 def izquierda(): # Giro a la izquierda
-    scGear.moveAngle(0,60)
-    move.move(30, 'forward', 'no', 1)
-
+    scGear.moveAngle(0,30)
+    time.sleep(0.1) 
+    move.move(20, 'forward', 'no', 1)
+   
+    
 def derecha(): # Giro a la derecha
-    scGear.moveAngle(0,-60)
-    move.move(30, 'forward', 'no', 1)
-
+    scGear.moveAngle(0,-30)
+    time.sleep(0.1)
+    move.move(20, 'forward', 'no', 1)
+   
+    
 def frente(): # Avance Frontal    
     scGear.moveAngle(0,0)
+    time.sleep(0.1)
     move.move(30, 'forward', 'no', 1)
 
 def detener(): # Paro de motores
@@ -39,47 +44,53 @@ def detener(): # Paro de motores
     
 # Programa principal
 def run():
+    while 1:
+        status_right = GPIO.input(line_pin_right)
+        status_middle = GPIO.input(line_pin_middle)
+        status_left = GPIO.input(line_pin_left)
+        
+        print("Valor Izquierda :: " + str(status_right) + " Valor Centro :: " + str(status_middle) + " Valor Derecha :: " + str(status_left))
+        
+        """
+        x = input()
 
-    status_right = GPIO.input(line_pin_right)
-    status_middle = GPIO.input(line_pin_middle)
-    status_left = GPIO.input(line_pin_left)
+        if x == 'w':
+            print("recto")
+            frente()
+        elif x == 'd':
+            print('derecha')
+            derecha()
+        elif x == 'a':
+            print('izquierda')
+            izquierda()
+        elif x =='s':
+            detener()
+            print('parar')
+        """
+        
+        """
+        Vista en primera persona
+        Limites de grados desde -60 hatsa 60 grados
+        """
 
-    print("Valor Izquierda :: " + str(status_right) + " Valor Centro :: " + str(status_middle) + " Valor Derecha :: " + str(status_left))
-    """
-    x = input()
-
-    if x == 'w':
-        print("recto")
-        frente()
-    elif x == 'd':
-        print('derecha')
-        derecha()
-    elif x == 'a':
-        print('izquierda')
-        izquierda()
-    elif x =='s':
-        detener()
-        print('parar')
-    """
-      
-    """
-    Vista en primera persona
-    Limites de grados desde -60 hatsa 60 grados
-    """
-
-    # Logica con 2 sensores
-    if status_right == 0 and status_left == 0: # (0 0)
-        frente()
-
-    if status_right == 0 and status_left == 1: # (0 1)
-        derecha()
-
-    if status_right == 1 and status_left == 0: # (1 0)
-        izquierda()
-
-    if status_right == 1 and status_left == 1: # (1 1)
-        frente()
-   
+        # Logica con 2 sensores
+       
+        if status_right == 0 and status_left == 0: # (0 0)
+            frente()
+            time.sleep(0.05)
+        if status_right == 0 and status_left == 1: # (0 1)
+            derecha()
+            time.sleep(0.05)
+        if status_right == 1 and status_left == 0: # (1 0)
+            izquierda()
+            time.sleep(0.05)
+        if status_right == 1 and status_left == 1: # (1 1)
+            frente()
+       
+        time.sleep(0.05)
+        
+        
+        # pass
     """
     bloque de comentario
     """
@@ -89,9 +100,10 @@ if __name__ == '__main__':
     try:
         setup()
         move.setup()
+        run()
         while 1:
-            run()
-        pass
+            pass    
+
     except KeyboardInterrupt:
         move.destroy()
         print("Saliendo")
